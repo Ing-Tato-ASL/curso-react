49310ae36b4c943197aac77484cb6181d8298f14
armando.sanmiguel <armandosanmiguel@gmail.com>
2024-07-29 16:08:35 -0600

    Hook Form

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Contador from './components/Contador';
 import Jsx from './components/Jsx';
 import Lista from './components/Lista';
 import Formulario from './components/Formulario';
+import HookForm from './components/HookForm';
 
 function App() {
   return (
     <div className='container mt-5'>
       <h1>Mi primer página con React</h1>
-      <Formulario />
+      <HookForm />
     </div>
   );
 }
---
+++ src/components/HookForm.js
@@ -0,0 +1,41 @@
+import React, {Fragment} from 'react'
+import {useForm} from 'react-hook-form'
+import { ErrorMessage } from "@hookform/error-message"
+
+const HookForm = () => {
+    const {
+        register,
+        formState: {errors},
+        handleSubmit
+    } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
+    return (
+        <Fragment>
+            <h1>Formulario</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                    className='form-control my-2'
+                    {...register(
+                            'singleErrorInput',
+                            {
+                                required: 'Título obligatorio'
+                            }
+                        )
+                    }
+                />
+                <ErrorMessage
+                  errors={errors}
+                  name="singleErrorInput"
+                  render={({ message }) => <p>{message}</p>}
+                />
+                <button className='btn btn-primary'>Enviar</button>
+            </form>
+        </Fragment>
+    );
+}
+ 
+export default HookForm;
