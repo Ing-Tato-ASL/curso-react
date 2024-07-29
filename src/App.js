cc873f12d8a0da1206e2064deb001098501e0d76
armando.sanmiguel <armandosanmiguel@gmail.com>
2024-07-29 13:22:25 -0600

    Form

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react'
 import Contador from './components/Contador';
 import Jsx from './components/Jsx';
 import Lista from './components/Lista';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
-    <div className="App">
+    <div className='container mt-5'>
       <h1>Mi primer página con React</h1>
-      <Jsx />
-      <Lista />
+      <Formulario />
     </div>
   );
 }
