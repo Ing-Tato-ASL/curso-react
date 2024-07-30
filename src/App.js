e8176fb2e13f7620d6e54825b146f69e1e207f29
armando.sanmiguel <armandosanmiguel@gmail.com>
2024-07-30 12:04:35 -0600

    Componentes comentarios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react'
-import EjemploUno from './components/EjemploUno';
+import Saludo from './components/Saludo';
+import Comentario from './components/Comentario';
 
 function App() {
+    const sujeto = [
+        {
+            nombre: 'Juanito',
+            urlImagen: 'https://via.placeholder.com/64',
+            texto: '1. Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+        },
+        {
+            nombre: 'Pedrito',
+            urlImagen: 'https://via.placeholder.com/64',
+            texto: '2. Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+        }
+    ];
+
   return (
     <div className='container mt-5'>
       <h1>Mi primer página con React</h1>
-      <EjemploUno />
+      <Saludo persona='Juanito' />
+      <Saludo persona='Ignacio' />
+      <Saludo persona='Catalina' />
+      <Comentario sujeto={sujeto[0]} />
+      <Comentario sujeto={sujeto[1]} />
     </div>
   );
 }
